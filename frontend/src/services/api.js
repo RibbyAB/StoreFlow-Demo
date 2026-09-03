5bc27953f5653709d2d96d34901de14d28b36c0b
Ribby <auliaribby@gmail.com>
2026-09-03 22:15:32 +0700

    domain change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -13,7 +13,7 @@ const isLocalNetwork =
 
 const API_BASE_URL = isLocalNetwork
   ? `http://${hostname}:8080/api`
-  : 'https://store-flow-two.vercel.app/api';
+  : 'https://store-flow-demo.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
